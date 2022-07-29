4fb2d796a25394fecde14084ac9ac6ff190fd6f1
alexey-z-m <alexey.panda@gmail.com>
2022-07-29 11:21:35 +0300

    getData from marvel

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,28 +1,39 @@
 import UIKit
 import PlaygroundSupport
+import CryptoKit
 
 func getData(urlRequest: String) {
-    let urlRequest = URL(string: urlRequest)
-    
+    guard let url = URL(string: urlRequest) else { return }
     let configuration: URLSessionConfiguration = .default
     configuration.allowsCellularAccess = true
     configuration.waitsForConnectivity = true
-    configuration.timeoutIntervalForRequest = 30
-    
     let session = URLSession(configuration: configuration)
-    
-    guard let url = urlRequest else { return }
-    
-    session.dataTask(with: url) { data, response, error in
+    session.dataTask(with: url) { (data, response, error) in
         if error != nil {
-            print(error?.localizedDescription ?? "eR")
-        } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
-            guard let data = data else { return }
+            print(error?.localizedDescription ?? "")
+        } else { print("ошибок нет") }
+        if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            print(response)
+        }
+        if let data = data {
             let dataAsString = String(data: data, encoding: .utf8)
-            print(dataAsString ?? "...")
-            print(response.statusCode)
+            print(dataAsString ?? "nil")
         }
     }.resume()
 }
-getData(urlRequest: "https://cataas.com/cat/says/Mobile%20Dev%20Factoty%20student")
+
+func MD5(string: String) -> String {
+    let digest = Insecure.MD5.hash(data: string.data(using: .utf8) ?? Data())
+    return digest.map {
+        String(format: "%02hhx", $0)
+    }.joined()
+}
+
+let publikAPIKey = "a0a684568b19c5174e95b5d14f5c3caa"
+let privateAPIKey = "4bd938fd97d1eb93edb93aad6304d87a531fbd4f"
+let timeStamp = "123"
+let toHash = timeStamp + privateAPIKey + publikAPIKey
+let baseUrl = "https://gateway.marvel.com/v1/public/characters/1011010?ts=" + timeStamp
+let url = baseUrl + "&apikey=" + publikAPIKey + "&hash=" + MD5(string: toHash)
+getData(urlRequest: url)
 
